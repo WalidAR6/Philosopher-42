7ee789346f0aeb92f6a7d08b07874663d085fcc3
Walid Araissi <waraissi@e2r6p6.1337.ma>
2023-03-31 00:56:09 +0000

    mn jdid

---
+++ philo/inc/philo.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: waraissi <waraissi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/31 00:38:37 by waraissi          #+#    #+#             */
+/*   Updated: 2023/03/31 00:48:09 by waraissi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+/*Include Librairies*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <sys/time.h>
+
+/*Declare Structs*/
+typedef struct s_philo
+{
+	int			id;
+	pthread_t	th;
+
+}
+
+
+
+
+/*Link Functions*/
+
+
+#endif
