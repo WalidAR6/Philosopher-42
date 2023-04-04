21f1e406539b0f3b0b4af3217e3ed98f46d4c5e1
Walid Araissi <waraissi@e2r10p1.1337.ma>
2023-04-04 17:01:28 +0000

    okeey

--- philo/src/actions.c
+++ philo/src/actions.c
@@ -6,7 +6,7 @@
 /*   By: waraissi <waraissi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/01 02:18:20 by waraissi          #+#    #+#             */
-/*   Updated: 2023/04/04 15:21:56 by waraissi         ###   ########.fr       */
+/*   Updated: 2023/04/04 16:48:19 by waraissi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,6 @@
 
 void	is_eating(t_philo *vars, int i)
 {
-	if (vars->info->num_philo == 1)
-	{
-		pthread_mutex_lock(&vars->info->fork[vars->right_fork]);
-		put_logs(vars, vars->id, "has take the right fork");
-		pthread_mutex_unlock(&vars->info->fork[vars->right_fork]);
-	}
-	else
-	{
 		pthread_mutex_lock(&vars->info->fork[vars->right_fork]);
 		put_logs(vars, vars->id, "has take the right fork");
 		pthread_mutex_lock(&vars->info->fork[vars->left_fork]);
@@ -32,7 +24,6 @@ void	is_eating(t_philo *vars, int i)
 		pthread_mutex_unlock(&vars->info->fork[vars->right_fork]);
 		pthread_mutex_unlock(&vars->info->fork[vars->left_fork]);
 		vars->num_of_eat++;	
-	}
 }
 
 void	is_thinking(t_philo *vars, int i)
--- philo/src/routine.c
+++ philo/src/routine.c
@@ -6,7 +6,7 @@
 /*   By: waraissi <waraissi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/31 17:54:50 by waraissi          #+#    #+#             */
-/*   Updated: 2023/04/04 02:33:39 by waraissi         ###   ########.fr       */
+/*   Updated: 2023/04/04 16:49:38 by waraissi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,9 +28,17 @@ void	*routine(void *arg)
 		usleep(100);
 	while (1)
 	{
-		is_eating(vars, vars->id);
-		is_sleeping(vars, vars->id);
-		is_thinking(vars, vars->id);
+		if (vars->info->num_philo == 1)
+		{
+			pthread_mutex_lock(&vars->info->fork[vars->right_fork]);
+			put_logs(vars, vars->id, "has take the right fork");
+		}
+		else
+		{
+			is_eating(vars, vars->id);
+			is_sleeping(vars, vars->id);
+			is_thinking(vars, vars->id);
+		}
 	}
 	return (NULL);
 }
