532601b17c11bbb88b7a6280725e3f3e76abf946
Walid Araissi <waraissi@e2r6p6.1337.ma>
2023-04-07 08:40:49 +0000

    add bonus directory

---
+++ philo_bonus/inc/philo_bonus.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo_bonus.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: waraissi <waraissi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/07 08:33:14 by waraissi          #+#    #+#             */
+/*   Updated: 2023/04/07 08:40:04 by waraissi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_BONUS_H
+# define PHILO_BONUS_H
+
+/*Include Librairies*/
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <pthread.h>
+# include <sys/time.h>
+
+/*Declare Structs*/
+
+
+
+
+
+
+
+
+
+
+
+/*Link Functions*/
+int	b_parser(char **av);
+
+#endif
